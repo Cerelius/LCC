68f6c7cb35e8dc65782b5a22fce1bbeaecd2a9a1
lumsdent <tyler.j.lumsden@gmail.com>
2024-04-12 17:12:54 -0400

    create sheet per match and per player

--- process_match_information.py
+++ process_match_information.py
@@ -2,8 +2,8 @@ import json
 import requests
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
+import pandas
+from datetime import date
 
 def main():
     # Accept user input for the file name
@@ -32,7 +32,21 @@ def fetch_match_data(match_id):
     headers = {"X-Riot-Token": api_key}
     url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}"
     response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch match data: {response.text}")
     return response.json()
+        
+def process_match_data(match_data):
+    match_information = {}
+    participant_information = []
+    
+    match_information["game_id"] = match_data["metadata"]["matchId"]
+    
+    for participant in match_data["info"]["participants"]:
+        participant_information.append(process_participant_data(participant))
+    match_information["participants"] = participant_information
+    build_excel_workbook(match_information)
+    return match_information
 
 def process_participant_data(participant_data):
     participant_information = {}
@@ -48,17 +62,20 @@ def process_participant_data(participant_data):
     participant_information["totalDamageDealtToChampions"] = participant_data["totalDamageDealtToChampions"]
     participant_information["visionScore"] = participant_data["visionScore"]
     return participant_information
-        
-def process_match_data(match_data):
-    match_information = {}
-    participant_information = []
-    
-    match_information["game_id"] = match_data["metadata"]["matchId"]
-    
-    for participant in match_data["info"]["participants"]:
-        participant_information.append(process_participant_data(participant))
-    match_information["participants"] = participant_information
-    
-    return match_information
 
+def build_excel_workbook(_match_data):
+    match_df = pandas.DataFrame(_match_data["participants"])
+    with pandas.ExcelWriter(f"results/{_match_data['game_id']}_{date.today()}.xlsx", engine="openpyxl", mode="w") as writer:
+        match_df.to_excel(writer, sheet_name=f"{_match_data['game_id']}", index=False)
+        for participant in _match_data["participants"]:
+            sheet_name = f"{participant['riotIdGameName']}"
+            if sheet_name in writer.sheets:
+                startrow = writer.sheets[sheet_name].max_row
+                df = pandas.DataFrame(participant, index=[0])
+                df.to_excel(writer, sheet_name=sheet_name, startrow=startrow, index=False, header=False)
+            else:
+                pandas.DataFrame(participant, index=[0]).to_excel(writer, sheet_name=sheet_name, index=False)
+
+
+load_dotenv(dotenv_path=".env", verbose=True, override=True)           
 main()
