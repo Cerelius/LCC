47d43f9f57b23a0c2149ff17ba7a7a9948fc7ed5
lumsdent <tyler.j.lumsden@gmail.com>
2024-04-26 22:54:24 -0400

    One page Match Reports

--- outputs.py
+++ outputs.py
@@ -7,6 +7,12 @@ def build_excel_workbook(matches):
     with pandas.ExcelWriter(f"results/Stats_Report_{date.today()}.xlsx", engine="openpyxl", mode="w") as writer:
         for match in matches:
             pandas.DataFrame(match["participants"]).to_excel(writer, sheet_name=f"{match['game_id']}", index=False)
+            sheet_name = "Match Report"
+            if sheet_name in writer.sheets:
+                startrow = writer.sheets[sheet_name].max_row
+                pandas.DataFrame(match["participants"]).to_excel(writer, sheet_name=sheet_name, startrow=startrow, index=False, header=False)
+            else:
+                pandas.DataFrame(match["participants"]).to_excel(writer, sheet_name=sheet_name, index=False)
             for participant in match["participants"]:
                 sheet_name = f"{participant['riotIdGameName']}"
                 if sheet_name in writer.sheets:
